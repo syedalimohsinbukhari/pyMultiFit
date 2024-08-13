e2730632a8085203bf603b75e1dbe86f6596fb0d
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-08-14 02:30:15 +0500

    [0003-ParEXTRACTION-minor-8-2]
    1. minor corrections to `arcSine_d.py`

--- src/pymultifit/distributions/arcSine_d.py
+++ src/pymultifit/distributions/arcSine_d.py
@@ -1,11 +1,31 @@
 """Created on Aug 14 02:02:42 2024"""
 
+from typing import Optional
+
 from .beta_d import BetaDistribution
 
 
 class ArcSineDistribution(BetaDistribution):
+    """
+    Represents the arcsine distribution as a special case of the Beta distribution.
 
-    def __init__(self, normalize: bool = True):
-        super().__init__(1, 0.5, 0.5, normalize)
+    The arcsine distribution is a special case of the Beta distribution with parameters
+    alpha = 0.5 and beta = 0.5.
+    """
 
-        self.norm = normalize
+    def __init__(self, amplitude: Optional[float] = 1., normalize: bool = True):
+        """
+        Initializes the arcsine distribution as a special case of the Beta distribution.
+
+        Parameters
+        ----------
+        amplitude: float, optional
+            Scaling factor for non-normalized arcsine distribution (default is 1).
+        normalize : bool, optional
+            Determines if the distribution should be normalized (default is True).
+
+        Notes
+        ------
+            If normalize flag is set to True, it ignores the amplitude scaling.
+        """
+        super().__init__(amplitude, 0.5, 0.5, normalize)
