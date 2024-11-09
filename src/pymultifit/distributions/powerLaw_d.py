91f32ed8eb194307ffda26012ddda940f3d297ea
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-11-10 00:23:22 +0500

    [development-minor-7]
    1. dirty implementaion of powerlaw model

---
+++ src/pymultifit/distributions/powerLaw_d.py
@@ -0,0 +1,43 @@
+"""Created on Nov 10 00:12:52 2024"""
+
+from typing import Any, Dict
+
+import numpy as np
+
+from ._backend import BaseDistribution
+
+
+class PowerLawDistribution(BaseDistribution):
+    
+    def __init__(self, alpha):
+        self.alpha = alpha
+        
+        self.norm = True
+        self.amplitude = 1.
+    
+    @classmethod
+    def with_amplitude(cls, amplitude: float = 1, alpha: float = -1):
+        instance = cls(alpha=alpha)
+        instance.amplitude = amplitude
+        instance.norm = False
+        
+        return instance
+    
+    def _pdf(self, x: np.ndarray):
+        return power_law_(x, amplitude=self.amplitude, alpha=self.alpha, normalize=self.norm)
+    
+    def pdf(self, x: np.ndarray) -> np.ndarray:
+        return self._pdf(x)
+    
+    def cdf(self, x: np.ndarray) -> np.ndarray:
+        pass
+    
+    def stats(self) -> Dict[str, Any]:
+        pass
+
+
+def power_law_(x: np.ndarray, amplitude: float = 1, alpha: float = -1, normalize: bool = True):
+    return amplitude * x**-alpha
+
+
+powerLawWA = PowerLawDistribution.with_amplitude
---
+++ src/pymultifit/fitters/powerLaw_f.py
@@ -0,0 +1,38 @@
+"""Created on Nov 10 00:17:16 2024"""
+
+import numpy as np
+
+from ._backend.baseFitter import BaseFitter
+from ..distributions.powerLaw_d import powerLawWA
+
+
+class PowerLawFitter(BaseFitter):
+    
+    def __init__(self, n_fits: int, x_values, y_values, max_iterations: int = 1000):
+        super().__init__(n_fits=n_fits, x_values=x_values, y_values=y_values, max_iterations=max_iterations)
+        self.n_par = 2
+    
+    @staticmethod
+    def _fitter(x, params):
+        return powerLawWA(*params).pdf(x)
+    
+    def _n_fitter(self, x, *params):
+        y = np.zeros_like(x)
+        params = np.reshape(params, (self.n_fits, self.n_par))
+        for amp, alpha in params:
+            y += self._fitter(x=x, params=[amp, alpha])
+        return y
+    
+    def _plot_individual_fitter(self, x, plotter):
+        params = np.reshape(self.params, (self.n_fits, self.n_par))
+        for i, (amp, alpha) in enumerate(params):
+            plotter.plot(x, self._fitter(x=x, params=[amp, alpha]),
+                         '--', label=f'PowerLaw {i + 1}('
+                                     f'{self.format_param(amp)}'
+                                     f'{self.format_param(alpha)}')
+    
+    def _get_overall_parameter_values(self):
+        pass
+    
+    def parameter_extractor(self):
+        pass
