1ff5bd74cf59c64ddb5c6200155aa2e1ba413216
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-11-30 10:38:17 +0500

    [main-minor-5]
    1. numpy compatibility fix
    2. updated `requirements.txt`, `setup.py`, `version.py`, `environment.yaml` and `pyproject.toml`

--- src/pymultifit/version.py
+++ src/pymultifit/version.py
@@ -1,6 +1,6 @@
 """Created on Nov 02 21:24:07 2024"""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5a0"
 __author__ = "Syed Ali Mohsin Bukhari"
 __email__ = "syedali.b@outlook.com"
 __license__ = "MIT"
