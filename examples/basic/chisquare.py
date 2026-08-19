a2f62c8668846bed663011c18873373eb9c16618
abj-a11y <garconjaq@gmail.com>
2026-08-19 13:55:39 +0500

    Examples images for -johnsonSU -invchisquare

--- examples/basic/chisquare.py
+++ examples/basic/chisquare.py
@@ -6,28 +6,29 @@ from scipy.stats import chi2
 
 from pymultifit.distributions import ChiSquareDistribution
 
-x_values = np.linspace(start=0, stop=5, num=500)
+# Shared x domain so the location shift along the x-axis is accurately visualized
+x_values = np.linspace(start=0, stop=8, num=500)
 
-y_multifit = ChiSquareDistribution(degree_of_freedom=1, normalize=True)
+y_multifit = ChiSquareDistribution(degree_of_freedom=1, loc=0, normalize=True)
 y_scipy = chi2
 
 f, ax = plt.subplots(1, 2, figsize=(12, 5))
 
-ax[0].plot(x_values, y_scipy.pdf(x=x_values, df=1), label="Scipy Chi2")
+ax[0].plot(x_values, y_scipy.pdf(x=x_values, df=1, loc=0), label="Scipy Chi2")
 ax[0].plot(x_values, y_multifit.pdf(x_values), "k:", label="pyMultiFit Chi2")
 ax[0].set_ylabel("f(x)")
 
-ax[1].plot(x_values, y_scipy.cdf(x=x_values, df=1), label="Scipy Chi2")
+ax[1].plot(x_values, y_scipy.cdf(x=x_values, df=1, loc=0), label="Scipy Chi2")
 ax[1].plot(x_values, y_multifit.cdf(x_values), "k:", label="pyMultiFit Chi2")
 ax[1].set_ylabel("F(x)")
 
-f.suptitle(r"$\chi^2$(1)")
+f.suptitle(r"$\chi^2$(1, loc=0)")
 
 for i in ax:
     i.set_xlabel("X")
     i.legend()
 plt.tight_layout()
-# plt.savefig('./../../images/chi2_example1.png')
+plt.savefig("./../../images/chi2_example1.png")
 
 y_multifit = ChiSquareDistribution(degree_of_freedom=1, loc=3, normalize=True)
 
@@ -47,4 +48,5 @@ for i in ax:
     i.set_xlabel("X")
     i.legend()
 plt.tight_layout()
-# plt.savefig('./../../images/chi2_example2.png')
+plt.savefig("./../../images/chi2_example2.png")
+plt.show()
